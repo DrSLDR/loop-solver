29cd77c90bcb31e387d3d5293246aa25042f947b
Jonas A. Hulten <jonas@sldr.se>
2015-10-31 00:54:52 +0100

    Added a new, supposed-to-fail test. For testing purposes.

--- Tests.hs
+++ Tests.hs
@@ -14,5 +14,10 @@ main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
 -- Test-test. Shows that a double-reversed list is identical to the identity
 prop_doubleReverse s = (reverse . reverse) s == id s
   where _ = s :: [Int]
+
+-- Test-test. Intended to fail
+prop_intentionalFailure s = reverse s == id s
+  where _ = s :: [Int]
 {------------------------------------------------------------------------------}
-tests = [("doubleReverse/id", quickCheck prop_doubleReverse)]
+tests = [("doubleReverse/id", quickCheck prop_doubleReverse),
+         ("intentionalFailure", quickCheck prop_intentionalFailure)]
