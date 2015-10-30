db648cc8dfa558e451b1464ab78ae4ad34c54e57
Jonas A. Hulten <jonas@sldr.se>
2015-10-31 00:15:49 +0100

    Added Travis integration and some simple tests.

---
+++ Tests.hs
@@ -0,0 +1,18 @@
+{-
+-- Tests.hs
+-- Contains all the tests for the loop-solver project and some test-tests.
+--
+-- Fundamentals unceremoniously stolen/borrowed from:
+--  https://github.com/spockz/TravisHSTest/blob/master/Tests.hs
+-}
+
+import Test.QuickCheck
+import Text.Printf
+
+main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
+{------------------------------------------------------------------------------}
+-- Test-test. Shows that a double-reversed list is identical to the identity
+prop_doubleReverse s = (reverse . reverse) s == id s
+  where _ = s :: [Int]
+{------------------------------------------------------------------------------}
+tests = [("doubleReverse/id", quickCheck prop_doubleReverse)]
