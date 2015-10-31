72a57717f7dd1d54b29e932349f9b6417f42609f
Jonas A. Hulten <jonas@sldr.se>
2015-10-31 01:02:02 +0100

    Updated the tests with type declarations.
    Hoping that will help with the supposed-to-fail test succeeding.

--- Tests.hs
+++ Tests.hs
@@ -9,15 +9,17 @@
 import Test.QuickCheck
 import Text.Printf
 
+main :: IO ()
 main = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
 {------------------------------------------------------------------------------}
 -- Test-test. Shows that a double-reversed list is identical to the identity
+prop_doubleReverse :: [Int] -> Bool
 prop_doubleReverse s = (reverse . reverse) s == id s
-  where _ = s :: [Int]
 
 -- Test-test. Intended to fail
+prop_intentionalFailure :: [Int] -> Bool
 prop_intentionalFailure s = reverse s == id s
-  where _ = s :: [Int]
 {------------------------------------------------------------------------------}
+tests :: [(String, IO ())]
 tests = [("doubleReverse/id", quickCheck prop_doubleReverse),
          ("intentionalFailure", quickCheck prop_intentionalFailure)]
